2d74706434e5edcae835d780521aa29dc76b3edc
guanyi.bei <guanyi.bei@cyphertech.cn>
2016-10-27 10:44:21 +0800

    tijiao

--- src/server/private/upload.js
+++ src/server/private/upload.js
@@ -2,101 +2,98 @@ var formidable = require('formidable')
 var util = require('util')
 var fs = require('fs')
 
-var dealFile = function(fileInfo) {
-    var file = require('../../db/models/file')
-    var attachment = require('../../db/models/attachment')
-    return file.upsert({
-        hash: fileInfo.hash,
-        size: fileInfo.size,
-        path: "upload/files/" + fileInfo.hash,
-        type: fileInfo.type
-    }).then(() => {
-        return attachment.create({
-            file_hash: fileInfo.hash,
-            name: fileInfo.name
-        })
-    })
-}
-
-var file = function(req) {
-    return new Promise(function(resolve, reject) {
-        if (req.method.toLowerCase() == 'post') {
-            var form = new formidable.IncomingForm();
-            form.uploadDir = "upload/temp";
-            form.maxFieldsSize = 2; //10G
-            form.hash = "md5"
-
-            if (!fs.existsSync("upload")) {
-                fs.mkdirSync("upload")
-            }
-
-            if (!fs.existsSync("upload/files")) {
-                fs.mkdirSync("upload/files")
-            }
-            if (!fs.existsSync("upload/temp")) {
-                fs.mkdirSync("upload/temp")
-            }
-
-            form.on('file', function(name, file) {
-                fs.rename(file.path, "upload/files/" + file.hash, function(result) {
-                    dealFile(file).then((result) => {
-                        resolve(result)
-                    })
-                })
-            })
-            form.on('error', function(err) {
-                console.log('error' + err)
-            })
-            form.parse(req)
-        } else {
-            reject("please post")
-        }
-    })
-}
-
-var getAttachment = function(req, res, next) {
-    var id = req.query.id
-    if (id) {
-        var fs = require('fs')
+var exec = {
+    dealFile(fileInfo) {
         var file = require('../../db/models/file')
         var attachment = require('../../db/models/attachment')
-        attachment.belongsTo(file)
-        attachment.findOne({
-            include: file,
-            where: {
-                id: id
-            }
-        }).then((result) => {
-            if (result != null) {
-                var localFile = fs.readFileSync("upload/files/" + result.file_hash, 'binary')
-                res.setHeader('Content-disposition', 'inline; filename=' + encodeURIComponent(result.name))
-                res.setHeader('Content-Type', result.file.type)
-                res.setHeader('Content-Length', result.file.size)
-                res.write(localFile, 'binary')
-                res.end()
+        return file.upsert({
+            hash: fileInfo.hash,
+            size: fileInfo.size,
+            path: "upload/files/" + fileInfo.hash,
+            type: fileInfo.type
+        }).then(() => {
+            return attachment.create({
+                file_hash: fileInfo.hash,
+                name: fileInfo.name
+            })
+        })
+    },
+    getAttachment(req, res, next) {
+        var id = req.query.id
+        if (id) {
+            var fs = require('fs')
+            var file = require('../../db/models/file')
+            var attachment = require('../../db/models/attachment')
+            attachment.belongsTo(file)
+            attachment.findOne({
+                include: file,
+                where: {
+                    id: id
+                }
+            }).then((result) => {
+                if (result != null) {
+                    var localFile = fs.readFileSync("upload/files/" + result.file_hash, 'binary')
+                    res.setHeader('Content-disposition', 'inline; filename=' + encodeURIComponent(result.name))
+                    res.setHeader('Content-Type', result.file.type)
+                    res.setHeader('Content-Length', result.file.size)
+                    res.write(localFile, 'binary')
+                    res.end()
+                } else {
+                    return Promise.reject("no file record")
+                }
+            })
+        } else {
+            return Promise.reject("no file id")
+        }
+    },
+    file(req) {
+        return new Promise(function(resolve, reject) {
+            if (req.method.toLowerCase() == 'post') {
+                var form = new formidable.IncomingForm();
+                form.uploadDir = "upload/temp";
+                form.maxFieldsSize = 2; //10G
+                form.hash = "md5"
+
+                if (!fs.existsSync("upload")) {
+                    fs.mkdirSync("upload")
+                }
+
+                if (!fs.existsSync("upload/files")) {
+                    fs.mkdirSync("upload/files")
+                }
+                if (!fs.existsSync("upload/temp")) {
+                    fs.mkdirSync("upload/temp")
+                }
+
+                form.on('file', function(name, file) {
+                    fs.rename(file.path, "upload/files/" + file.hash, function(result) {
+                        dealFile(file).then((result) => {
+                            resolve(result)
+                        })
+                    })
+                })
+                form.on('error', function(err) {
+                    console.log('error' + err)
+                })
+                form.parse(req)
             } else {
-                return Promise.reject("no file record")
+                reject("please post")
             }
         })
-    } else {
-        return Promise.reject("no file id")
     }
 }
+
 
 module.exports = (req, res, next) => {
     var action = req.params.action
     Promise.resolve(action).then(function(result) {
-        switch (result) {
-            case "file":
-                return file(req)
-            case 'getAttachment':
-                return getAttachment(req, res, next)
-        }
+        return exec[result](req, res, next)
     }).then(function(result) {
         if (req.params.action != "getAttachment") {
             res.send(result)
         }
     }).catch(function(error) {
+        console.log(error)
         res.status(500).send(error.toString())
     })
 }
